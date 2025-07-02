c173042f800932debf249f47a72d176367446e66
iqbal125 <iqbal125sf@gmail.com>
2025-07-02 09:34:36 -0700

    add alembic and postgres

--- app/globals/db.py
+++ app/globals/db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.globals.settings import settings
 
 DATABASE_URL = settings.DB_URL
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+    DATABASE_URL, echo=True
 )
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
